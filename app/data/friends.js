7ecf1cc3645366d56b7438ea4066d0259dadb970
unknown <nicole@popflier.com>
2018-04-09 13:10:06 -0600

    adding final files

---
+++ app/data/friends.js
@@ -0,0 +1,104 @@
+
+
+var friends = [
+  {
+    name: "mark",
+    photo: "http://via.placeholder.com/250x250.jpg",
+    scores: [
+      "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
+  },
+  {
+    name: "john",
+    photo: "http://via.placeholder.com/250x250.jpg",
+    scores: [
+      "4",
+      "2",
+      "5",
+      "1",
+      "3",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2"
+    ]
+  },
+  {
+    name: "sloan",
+    photo: "http://via.placeholder.com/250x250.jpg",
+    scores: [
+      "5",
+      "2",
+      "2",
+      "2",
+      "4",
+      "1",
+      "3",
+      "2",
+      "5",
+      "5"
+    ]
+  },
+  {
+    name: "tyler",
+    photo: "http://via.placeholder.com/250x250.jpg",
+    scores: [
+      "3",
+      "3",
+      "4",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    name: "adam",
+    photo: "http://via.placeholder.com/250x250.jpg",
+    scores: [
+      "4",
+      "3",
+      "4",
+      "1",
+      "5",
+      "2",
+      "5",
+      "3",
+      "1",
+      "4"
+    ]
+  },
+  {
+    name: "bono",
+    photo: "http://via.placeholder.com/250x250.jpg",
+    scores: [
+      "4",
+      "4",
+      "2",
+      "3",
+      "2",
+      "2",
+      "3",
+      "2",
+      "4",
+      "5"
+    ]
+  }
+
+];
+
+
+module.exports = friends;
